82149f7ad3b79ad5a7c35f5a5c74d8afbb0e09c6
Rafal Kukawski <rafal.kukawski@external.t-mobile.pl>
2024-11-22 17:26:46 +0100

    refactor: Optimize CSV writing with BufWriter and add flush method

--- src/csv_writer.rs
+++ src/csv_writer.rs
@@ -1,19 +1,26 @@
 use std::fs::File;
 use std::io::{self, Write};
 
+use std::io::{self, Write, BufWriter};
+
 pub struct CsvWriter {
-    file: File,
+    writer: BufWriter<File>,
 }
 
 impl CsvWriter {
     pub fn new(file_path: &str) -> io::Result<Self> {
         let file = File::create(file_path)?;
-        Ok(CsvWriter { file })
+        let writer = BufWriter::new(file);
+        Ok(CsvWriter { writer })
     }
 
     pub fn write_row(&mut self, data: &[&str]) -> io::Result<()> {
         let row = data.join(",") + "\n";
-        self.file.write_all(row.as_bytes())
+        self.writer.write_all(row.as_bytes())
+    }
+
+    pub fn flush(&mut self) -> io::Result<()> {
+        self.writer.flush()
     }
 }
 
